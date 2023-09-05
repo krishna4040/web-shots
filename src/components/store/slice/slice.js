a0b688b002dd89a177f23ab4f5fb107d5bb59019
krishna jain <115697713+krishna4040@users.noreply.github.com>
2023-09-05 12:45:19 +0530

    redux logic added

---
+++ src/components/store/slice/slice.js
@@ -0,0 +1,17 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+const slice = createSlice({
+    name: "liked",
+    initialState: [],
+    reducers: {
+        like : (state,action) => {
+            state.push(action.payload);
+        },
+        unlike : (state,action) => {
+            state.filter(item => item.id !== action.payload);
+        }
+    }
+});
+
+export const {like,unlike} = slice.actions;
+export default slice.reducer;
