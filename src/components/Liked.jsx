cd6bf1f51b882d35870a9e14e26caf8322986522
krishna jain <115697713+krishna4040@users.noreply.github.com>
2023-09-08 01:07:18 +0530

    Like feature completed succsesfully

--- src/components/Liked.jsx
+++ src/components/Liked.jsx
@@ -3,19 +3,19 @@ import Card from './Card'
 export default function Liked({liked}) {
 
     return(
-        <div>
+        <div className='grid items-center justify-center grid-cols-1 gap-5 p-5 lg:grid-cols-3'>
             {
                 liked.map((element , index) => {
                     return (<Card
                         author={element.author}
                         content={element.content}
-                        desc={element.description}
-                        date={element.publishedAt}
-                        source={element.source.name}
+                        desc={element.desc}
+                        date={element.date}
+                        source={element.source}
                         title={element.title}
                         url={element.url}
-                        img={element.urlToImage}
-                        id={index}
+                        img={element.img}
+                        id={element.id}
                         key={index}
                     />)
                 })
